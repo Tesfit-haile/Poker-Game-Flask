c6f081475f5503bdf102a7a5b33519f8498c20be
Tesfit12 <menkregrma12@gmail.com>
2022-05-29 06:08:23 +0300

    poker game flask app added

---
+++ poker_back_end.py
@@ -0,0 +1,61 @@
+import random
+
+
+def generate_cards(number):
+    value=["1","2","3","4","5","6","7","8","9","10","J","Q","K"]
+    symbol=["♣","♦","♥","♠"]
+
+    gen_card=[]
+
+    for i in range(number):
+        random_value=random.choice(value)
+        random_symbol=random.choice(symbol)
+
+        while (random_value,random_symbol) in gen_card:
+            random_value = random.choice(value)
+            random_symbol = random.choice(symbol)
+
+        gen_card.append((random_value,random_symbol))
+    return gen_card
+
+
+class Player():
+    active_player=1
+
+
+    @classmethod
+    def move_to_next_player(cls,list_players):
+        index_active=cls.active_player-1
+        index_next_player=(index_active+1)%len(list_players)
+
+        i=index_next_player
+        while True:
+            if list_players[i].is_in:
+                index_next_player = i
+                break
+            else:
+                i=(i+1)%4
+
+        cls.active_player=index_next_player+1
+
+
+    def __init__(self,name,chips,avatar,id):
+        self.name=name
+        self.chips=chips
+        self.avatar=avatar
+        self.cards=generate_cards(2)
+        self.is_in=True
+        self.id=id
+
+    def bet_money(self,chips):
+        self.chips=self.chips-chips
+
+    def win_game(self,chips):
+        self.chips=self.chips+chips
+
+    def fold(self):
+        self.is_in=False
+
+
+
+players=[]
---
+++ routes.py
@@ -0,0 +1,56 @@
+import flask
+from application_poker import app
+import application_poker.poker_back_end as back_end
+import application_poker.forms
+
+
+
+@app.route('/poker', methods=('GET', 'POST'))
+def launch_poker_game():
+    formActionPlayer = application_poker.forms.ActionPlayer()
+
+
+    if formActionPlayer.validate_on_submit():
+        if formActionPlayer.fold.data:
+            print("Fold")
+        if formActionPlayer.call.data:
+            print("Call")
+        if formActionPlayer.raisee.data:
+            print("Raise")
+
+        back_end.Player.move_to_next_player(back_end.players)
+
+
+
+
+    cards=back_end.generate_cards(3)
+    return flask.render_template('poker_game.html',players=back_end.players,cards=cards, Player=back_end.Player, formActionPlayer=formActionPlayer)
+
+
+@app.route("/",  methods=('GET', 'POST'))
+def config_game():
+    form=application_poker.forms.ConfigGame()
+
+    if form.validate_on_submit():
+        back_end.players=[]
+
+        player=back_end.Player(form.name_1.data,int(form.chips_1.data),form.avatar_1.data,1)
+        back_end.players.append(player)
+
+        player=back_end.Player(form.name_2.data,int(form.chips_2.data),form.avatar_2.data,2)
+        back_end.players.append(player)
+
+        player=back_end.Player(form.name_3.data,int(form.chips_3.data),form.avatar_3.data,3)
+        back_end.players.append(player)
+
+        player=back_end.Player(form.name_4.data,int(form.chips_4.data),form.avatar_4.data,4)
+        back_end.players.append(player)
+
+
+        return flask.redirect(flask.url_for('launch_poker_game'))
+    else:
+        return flask.render_template('config.html',form=form)
+
+
+
+
